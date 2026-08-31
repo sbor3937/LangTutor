207c67578e61605772b8c0dd3568b87aec4d2585
borisenko <borisenko@spbec-mining.ru>
2026-09-01 00:10:07 +0300

    Allow database owner to migrate FORCE RLS tables

--- deploy/postgres/bootstrap-roles.sql
+++ deploy/postgres/bootstrap-roles.sql
@@ -1,7 +1,7 @@
 -- Run once as a PostgreSQL cluster administrator. Passwords are assigned
 -- separately by the secret manager and must never be added to this file.
 DO $$ BEGIN
-  CREATE ROLE langtutor_owner NOLOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE NOREPLICATION;
+  CREATE ROLE langtutor_owner NOLOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE NOREPLICATION BYPASSRLS;
 EXCEPTION WHEN duplicate_object THEN NULL; END $$;
 DO $$ BEGIN
   CREATE ROLE langtutor_migrator NOLOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE NOREPLICATION;
--- deploy/postgres/init-roles.sh
+++ deploy/postgres/init-roles.sh
@@ -7,6 +7,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
   --set=database_name="$POSTGRES_DB" --set=runtime_password="$LANGTUTOR_RUNTIME_PASSWORD" --set=migration_password="$LANGTUTOR_MIGRATION_PASSWORD" <<'SQL'
 ALTER ROLE langtutor_runtime LOGIN PASSWORD :'runtime_password';
 ALTER ROLE langtutor_migrator LOGIN PASSWORD :'migration_password';
+ALTER ROLE langtutor_owner BYPASSRLS;
 GRANT CONNECT ON DATABASE :"database_name" TO langtutor_runtime, langtutor_migrator, langtutor_authenticator;
 GRANT CREATE ON DATABASE :"database_name" TO langtutor_owner;
 SQL
